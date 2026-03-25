b5c7442da64e2784d767b624af6ea1e723d06198
soyeon <sandy6317@naver.com>
2026-03-25 23:28:46 +0900

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,17 @@
+def greeting(name):
+ print("Hello, " + name)
+
+person1 = {
+ "name": "John",
+ "age": 36,
+ "country": "Norway"
+}
+
+
+def greeting(name):
+ print("Hello, " + name)
+person1 = {
+ "name": "John",
+ "age": 36,
+ "country": "Norway"
+}
